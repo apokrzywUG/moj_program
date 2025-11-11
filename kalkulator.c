b4b6d764f6639ecf483e951909780bbce65ffcd1
apokrzywUG <a.pokrzywinski.476@studms.ug.edu.pl>
2025-11-12 00:33:33 +0100

    Program kalkulator

---
+++ kalkulator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int a=10, b=5;
+	printf("a = %d, b = %d\n", a, b);
+	printf("Dodawanie: %d + %d = %d\n", a, b, a+b);
+	printf("Odejmowanie: %d - %d = %d\n", a, b, a-b);
+	printf("Mnożenie: %d * %d = %d\n", a, b, a*b);
+	printf("Dzielenie: %d / %d = %d\n", a, b, a/b);
+	return 0;
+}
