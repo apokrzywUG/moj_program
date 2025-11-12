95b0a2e2b2667b6d213e6c5c22bf8853f6e94e26
apokrzywUG <a.pokrzywinski.476@studms.ug.edu.pl>
2025-11-12 22:49:18 +0100

    Program liczacy srednia

---
+++ srednia.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+int main() {
+	int a=10, b=20, c=30;
+	float srednia = (a+b+c)/3;
+	printf("a=%d, b=%d, c=%d\n", a, b, c);
+	printf("Srednia arytmentyczna: %f\n", srednia);
+	return 0;
+}
